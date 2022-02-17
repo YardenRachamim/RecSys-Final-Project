db0af5c50f84eca7114a76e561b0b71531d8682a
Yarden <yardenrachamim1993@gmail.com>
2022-02-17 14:42:26 +0200

    Define the basic structere

---
+++ src/data/datasets.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+
+
+class Dataset(object):
+    def __init__(self, path, sep=',', session_key='SessionID', item_key='ItemID', time_key='Time', n_sample=-1, itemmap=None, itemstamp=None, time_sort=False):
+        # Read csv
+        self.df = pd.read_csv(path, sep=sep, dtype={session_key: int, item_key: int, time_key: float})
+        self.session_key = session_key
+        self.item_key = item_key
+        self.time_key = time_key
+        self.time_sort = time_sort
+        if n_sample > 0:
+            self.df = self.df[:n_sample]
+
+        # Add colummn item index to data
+        self.add_item_indices(itemmap=itemmap)
+        """
+        Sort the df by time, and then by session ID. That is, df is sorted by session ID and
+        clicks within a session are next to each other, where the clicks within a session are time-ordered.
+        """
+        self.df.sort_values([session_key, time_key], inplace=True)
+        self.click_offsets = self.get_click_offset()
+        self.session_idx_arr = self.order_session_idx()
+
+    def add_item_indices(self, itemmap=None):
+        """
+        Add item index column named "item_idx" to the df
+        Args:
+            itemmap (pd.DataFrame): mapping between the item Ids and indices
+        """
+        if itemmap is None:
+            item_ids = self.df[self.item_key].unique()  # type is numpy.ndarray
+            item2idx = pd.Series(data=np.arange(len(item_ids)),
+                                 index=item_ids)
+            # Build itemmap is a DataFrame that have 2 columns (self.item_key, 'item_idx)
+            itemmap = pd.DataFrame({self.item_key: item_ids,
+                                    'item_idx': item2idx[item_ids].values})
+        self.itemmap = itemmap
+        self.df = pd.merge(self.df, self.itemmap, on=self.item_key, how='inner')
+
+    def get_click_offset(self):
+        """
+        self.df[self.session_key] return a set of session_key
+        self.df[self.session_key].nunique() return the size of session_key set (int)
+        self.df.groupby(self.session_key).size() return the size of each session_id
+        self.df.groupby(self.session_key).size().cumsum() retunn cumulative sum
+        """
+        offsets = np.zeros(self.df[self.session_key].nunique() + 1, dtype=np.int32)
+        offsets[1:] = self.df.groupby(self.session_key).size().cumsum()
+        return offsets
+
+    def order_session_idx(self):
+        if self.time_sort:
+            sessions_start_time = self.df.groupby(self.session_key)[self.time_key].min().values
+            session_idx_arr = np.argsort(sessions_start_time)
+        else:
+            session_idx_arr = np.arange(self.df[self.session_key].nunique())
+        return session_idx_arr
+
+    @property
+    def items(self):
+        return self.itemmap[self.item_key].unique()
